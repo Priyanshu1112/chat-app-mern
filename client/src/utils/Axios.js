68fb0384e5ed73533d65d0949a4748ece7b5f658
Priyanshu Singh Raghuwanshi <97700245+Priyanshu1112@users.noreply.github.com>
2024-08-27 02:00:46 +0530

    Update Axios.js

--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   //   baseURL: "http://localhost:5000/",
-  baseURL: "https://chat-app-mern-production-66ad.up.railway.app/",
+  baseURL: "https://positive-youth-production.up.railway.app/",
 });
 
 export default axiosInstance;
